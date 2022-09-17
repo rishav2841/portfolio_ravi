0f1a7a0a2c7cc1581e3573f5f52eb34ccf74bc22
Ravi Ranjan <113210738+rishav2841@users.noreply.github.com>
2022-09-17 22:41:00 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -34,12 +34,40 @@ cancelBtn.onclick = function(){
   scrollBtn.style.pointerEvents = "auto";
 }
 
-// Side Navigation Bar Close While We Click On Navigation Links
-let navLinks = document.querySelectorAll(".menu li a");
-for (var i = 0; i < navLinks.length; i++) {
-  navLinks[i].addEventListener("click" , function() {
-    navBar.classList.remove("active");
-    menuBtn.style.opacity = "1";
-    menuBtn.style.pointerEvents = "auto";
-  });
-}
+// // Side Navigation Bar Close While We Click On Navigation Links
+// let navLinks = document.querySelectorAll(".menu li a");
+// for (var i = 0; i < navLinks.length; i++) {
+//   navLinks[i].addEventListener("click" , function() {
+//     navBar.classList.remove("active");
+//     menuBtn.style.opacity = "1";
+//     menuBtn.style.pointerEvents = "auto";
+//   });
+// }
+
+
+const block = document.querySelectorAll('.block');
+window.addEventListener('load', function(){
+  block.forEach(item => {
+    let numElement = item.querySelector('.num');
+    let num = parseInt(numElement.innerText);
+    let count = 0;
+    let time = 2000 / num;
+    let circle = item.querySelector('.circle');
+    setInterval(() => {
+      if(count == num){
+        clearInterval();
+      } else {
+        count += 1;
+        numElement.innerText = count;
+      }
+    }, time)
+    circle.style.strokeDashoffset 
+      = 503 - ( 503 * ( num / 100 ));
+    let dots = item.querySelector('.dots');
+    dots.style.transform = 
+      `rotate(${360 * (num / 100)}deg)`;
+    if(num == 100){
+      dots.style.opacity = 0;
+    }
+  })
+});
